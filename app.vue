3c69251b3500b447d654331237dfc3adbe572d34
Anthony Chu <anthony@anthonychu.ca>
2022-04-08 23:44:50 -0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 @click="updateTime()">{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let message = ref('')
+async function updateTime() {
+  message.value = await $fetch('/api/hello')
+}
+await updateTime()
+setInterval(updateTime, 1000);
+</script>
---
+++ server/api/hello.ts
@@ -0,0 +1,1 @@
+export default (req, res) => 'Server time is ' + new Date().toLocaleTimeString()
